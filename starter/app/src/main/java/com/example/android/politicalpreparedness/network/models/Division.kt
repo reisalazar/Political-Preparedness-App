53f7f3b3012b26c8952ee3537a2af9384b2e0786
reisalazar <rrsb45@gmail.com>
2022-09-15 15:25:05 -0300

    Build Gradle

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/Division.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/Division.kt
@@ -3,6 +3,7 @@ package com.example.android.politicalpreparedness.network.models
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+
 @Parcelize
 data class Division(
         val id: String,
